c856e7a504fb8032342d963dbf04c359c8f8bc06
paul <paul.christophe6@gmail.com>
2013-08-26 11:53:35 -0400

    some work on the header and the landing page

---
+++ public/javascripts/main.js
@@ -0,0 +1,9 @@
+$(function(){
+	$('body').waypoint(function(direction) {
+		if(direction === 'down'){
+			$('#nav').addClass('not-top');
+		}else{
+			$('#nav').removeClass('not-top');
+		}
+	},{offset: -1});
+});
