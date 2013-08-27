03da7b89addfb023a51289bd29bfeeadc4705d8d
David Huerta <huertanix@opentil.com>
2013-08-26 23:35:31 -0400

    home and about controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -15,11 +15,13 @@ ArthackdayWeb::Application.routes.draw do
 
   resources :admin
 
+  resources :about
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'events#index'
+   root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
