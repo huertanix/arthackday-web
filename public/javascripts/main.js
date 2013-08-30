6ab9f9290abd9d59fac01bf35ccbd9fd71db6daf
paul <paul.christophe6@gmail.com>
2013-08-30 11:21:57 -0400

    landing page stuff

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,14 @@
 $(function(){
+	// for hero block
+	var windowHeight = $(window).height(); 
+	$('#landing-header').css({
+		'height': windowHeight
+	});
+	var eventCallout = $('.event-callout').height();
+	eventCallout = - (eventCallout / 2);
+	$('.event-callout').css({
+		'margin-top': eventCallout
+	});
 	
 	// $('body').waypoint(function(direction) {
 	// 	if(direction === 'down'){
