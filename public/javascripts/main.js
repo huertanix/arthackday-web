9f9d208450e4cef7f2250fd9ee53065b6991a66a
paul <paul.christophe6@gmail.com>
2013-08-30 00:22:28 -0400

    working on the footer and nav

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,10 +1,10 @@
 $(function(){
 	
-	$('body').waypoint(function(direction) {
-		if(direction === 'down'){
-			$('#nav').addClass('not-top');
-		}else{
-			$('#nav').removeClass('not-top');
-		}
-	},{offset: -1});
+	// $('body').waypoint(function(direction) {
+	// 	if(direction === 'down'){
+	// 		$('#nav').addClass('not-top');
+	// 	}else{
+	// 		$('#nav').removeClass('not-top');
+	// 	}
+	// },{offset: -1});
 });
