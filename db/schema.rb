d18e353b883be638d8196a098fb1d5b84769c4d2
paul <paul.christophe6@gmail.com>
2013-11-28 22:11:07 -0500

    added fit text js

--- db/schema.rb
+++ db/schema.rb
@@ -140,7 +140,6 @@ ActiveRecord::Schema.define(version: 20131128225812) do
   create_table "sponsors", force: true do |t|
     t.string   "name"
     t.string   "website"
-    t.string   "logo_uri"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "event_id"
