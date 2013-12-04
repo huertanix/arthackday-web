21694b549f81acafc35ab1ee72b8622e45a3c158
David Huerta <huertanix@opentil.com>
2013-12-03 22:09:19 -0500

    Featured press blurbs and sponsors for about page fixes #55.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131203050011) do
+ActiveRecord::Schema.define(version: 20131204020413) do
 
   create_table "events", force: true do |t|
     t.string   "city"
@@ -154,6 +154,7 @@ ActiveRecord::Schema.define(version: 20131203050011) do
   create_table "sponsors", force: true do |t|
     t.string   "name"
     t.string   "website"
+    t.string   "logo_uri"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "event_id"
@@ -161,6 +162,7 @@ ActiveRecord::Schema.define(version: 20131203050011) do
     t.string   "logo_content_type"
     t.integer  "logo_file_size"
     t.datetime "logo_updated_at"
+    t.boolean  "featured"
   end
 
   create_table "venues", force: true do |t|
