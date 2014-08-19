ece34a8ed3f68b9fb1b6e60d25cb08c278837fdc
David Huerta <huertanix@opentil.com>
2014-08-19 00:21:26 -0400

    Kittens restored for #178.

--- app/models/project.rb
+++ app/models/project.rb
@@ -9,7 +9,7 @@ class Project < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :description, :tag, :website, :github, :participant_ids, :event_id, :medium_attributes, :featured_thumbnail, :photos_attributes
-  has_attached_file :main_image, :styles => { :medium => "600x400", :thumb => "300x200>", :chibi => "100x75>" }, :default_url => "http://placekitten.com/300/200"
+  has_attached_file :main_image, :styles => { :medium => "600x400", :thumb => "300x200>", :chibi => "100x75>" }, :default_url => "http://placekitten.com/640/480"
 
   friendly_id :participants_with_project_name, use: :slugged
 
