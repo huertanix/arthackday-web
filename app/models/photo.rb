2bc39758abbf4caedb1439cfd51d895de9601ddc
David Huerta <huertanix@opentil.com>
2014-08-18 22:52:56 -0400

    Photo stuff to S3 might work but Mavericks done goofed on my env vars.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,6 @@
 class Photo < ActiveRecord::Base
   belongs_to :projects
-  attr_accessible :credit, :project_id
+  attr_accessible :image, :credit, :project_id
 
-  has_attached_file :image, :styles => { :medium => "600x400", :thumb => "300x200>", :chibi => "100x75>" }, :default_url => "http://placekitten.com/300/200"
+  has_attached_file :image, :styles => { :medium => "600x400", :thumb => "300x200>", :chibi => "100x75>" }, :default_url => "http://placekitten.com/640/480"
 end
