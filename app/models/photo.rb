ad69eb11163f23fe58544a5c954e6ab076a58169
David Huerta <huertanix@opentil.com>
2014-11-25 15:52:22 -0500

    RIP Placekitten. Resolves #199.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,5 +2,5 @@ class Photo < ActiveRecord::Base
   belongs_to :projects
   attr_accessible :image, :credit, :project_id
 
-  has_attached_file :image, :styles => { :medium => "600x400", :thumb => "300x200>", :chibi => "100x75>" }, :default_url => "http://placekitten.com/640/480"
+  has_attached_file :image, :styles => { :medium => "600x400", :thumb => "300x200>", :chibi => "100x75>" }
 end
