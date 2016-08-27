9c3e218a1a80c02b488b437a7705d9256c37b684
David Huerta <huertanix@opentil.com>
2016-08-27 18:07:25 -0400

    Changing ordering to showcase event in future closest to present.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def index
     @events = Event.all.sorted_by_date
+
+    @future_events = Array.new
+
+    @events.map { |event| @future_events.push event unless event.show_end < Date.new }
   end
 end
