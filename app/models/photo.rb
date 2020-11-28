2fa102a7ec10db926df686a2e9cc0fbea7bd2f3f
David Huerta <david@freedom.press>
2020-11-28 00:53:21 -0500

    Adds validation for Paperclip image uploads.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,5 +2,6 @@ class Photo < ApplicationRecord
   belongs_to :projects
   #attr_accessible :image, :credit, :project_id
 
-  has_attached_file :image, :styles => { :medium => "600x400", :thumb => "300x200>", :chibi => "100x75>" }
+  has_attached_file :image, :styles => { :medium => "600x400", :thumb => "300x200>", :chibi => "100x75>" }, s3_permissions: "public-read"
+  validates_attached_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
--- app/models/sponsor.rb
+++ app/models/sponsor.rb
@@ -1,7 +1,8 @@
 class Sponsor < ApplicationRecord
   belongs_to :event
   #attr_accessible :name, :website, :logo, :featured
-  has_attached_file :logo, :styles => { :thumb => "300x200>", :chibi => "100x75>" }, :default_url => "http://placekitten.com/300/200"
+  has_attached_file :logo, :styles => { :thumb => "300x200>", :chibi => "100x75>" }, :default_url => "http://placekitten.com/300/200", s3_permissions: "public-read"
+  validates_attached_content_type :logo, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
   scope :featured_in_about, -> { where(:featured => true) }
 end
